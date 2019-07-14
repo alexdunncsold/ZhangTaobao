71dd3a456ea5481f9990f0217e63f166dbc05241
Alex Dunn <alexdunnmobile@gmail.com>
2019-07-13 23:10:01 -0700

    Added sleep hack to prevent double-posting until I sort out an explicit-wait.

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ POST_PERMALINK = 'https://www.facebook.com/groups/{}/permalink/{}/'.format(GROUP
 driver.get(POST_PERMALINK)
 
 now = datetime.utcnow().replace(tzinfo=utc)
-while now < AUCTION_END + timedelta(seconds=5):
+while now < AUCTION_END + timedelta(seconds=15):
     now = datetime.utcnow().replace(tzinfo=utc)
     if now > AUCTION_END - timedelta(seconds=5):
         try:

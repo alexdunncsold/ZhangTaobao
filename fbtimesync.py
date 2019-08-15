7fedf96ddd8c423e5dea8e8266cd210555b7af56
Alex Dunn <alexdunnmobile@gmail.com>
2019-08-14 22:15:21 -0700

    Fixed delay determination function

--- fbtimesync.py
+++ fbtimesync.py
@@ -31,14 +31,14 @@ class FbTimeSync:
             mean_posting_delay = self.get_mean_posting_delay(webdriver, 10)
             self.maximal_delay = mean_posting_delay + timedelta(milliseconds=500)
             print(
-                f'    Mean posting delay = {mean_posting_delay.seconds * 1000 + mean_posting_delay.microseconds / 1000}ms')
+                f'    Mean posting delay = {self.timedelta_to_ms(mean_posting_delay)}ms')
 
         except RuntimeError as err:
             self.maximal_delay = timedelta(seconds=5)
             print(f'    Error when determining posting delay')
 
         print(
-            f'    Maximal delay set to {self.maximal_delay.seconds * 1000 + self.maximal_delay.microseconds / 1000}ms')
+            f'    Maximal delay set to {self.timedelta_to_ms(self.maximal_delay)}ms')
 
         # Navigate back to the auction page
         print(f'    Navigating back to {auction_url}')
@@ -60,20 +60,34 @@ class FbTimeSync:
             return math.ceil(td.seconds * 1000 + td.microseconds / 1000)
 
     def get_mean_posting_delay(self, webdriver, trials):
+        start_time = datetime.now()
+        abort_threshold = timedelta(minutes=10)  # todo store abort threshold in config in future
         delay_results_ms = []
-        for trial in range(0, trials):
-            posting_delay = self.get_posting_delay_datum(webdriver)
-            if posting_delay < timedelta(0):
-                delay_ms = self.timedelta_to_ms(posting_delay)
-            else:
-                delay_ms = self.timedelta_to_ms(posting_delay)
-            delay_results_ms.append(delay_ms)
+        try:
+            for trial in range(0, trials):
+                posting_delay = self.get_posting_delay_datum(webdriver)
+                if posting_delay < timedelta(0):
+                    delay_ms = self.timedelta_to_ms(posting_delay)
+                else:
+                    delay_ms = self.timedelta_to_ms(posting_delay)
+                delay_results_ms.append(delay_ms)
 
-        return timedelta(milliseconds=statistics.mean(delay_results_ms))
+                # Having completed at least one test, abort if the auction will end in tne near future
+                sync_time_elapsed = datetime.now() - start_time
+                auction_time_remaining = self.sync_threshold - sync_time_elapsed
+                if auction_time_remaining < abort_threshold:
+                    raise RuntimeError(
+                        f'Less than {abort_threshold.seconds / 60} minutes left in auction, aborting test after {len(
+                            delay_results_ms)} tests')
+        except RuntimeError as err:
+            print(f'    {err.__repr__()}')
+
+        return timedelta(milliseconds=statistics.mean(delay_results_ms)) if delay_results_ms else timedelta(
+            seconds=5)  # store safe value in config in future
 
     def get_posting_delay_datum(self, webdriver):
         # If this method is called without being on sync page, load sync page
-        if not self.group.name in webdriver.title:
+        if self.group.name not in webdriver.title:
             webdriver.get(self.url)
 
         accuracy_tolerance = 0

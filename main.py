34c0d311e823d3ddffa20c002b347b45f52b62a0
Alexander Dunn <alexdunnmobile@gmail.com>
2019-10-05 09:16:12 -0700

    Added sleep inhibitor for Windows Implement Factory and Linux support later

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from supervisor import Supervisor
+from WindowsSleepInhibitor import WindowsSleepInhibitor
+
+import sys
+
+osSleep = None
+# in Windows, prevent the OS from sleeping while we run
+if sys.platform == 'win32':
+    osSleep = WindowsSleepInhibitor()
+    osSleep.inhibit()
 
 supervisor = Supervisor(dev=True, archive=False)
 supervisor.run()
+
+# Remove sleep inhibition
+if osSleep:
+    osSleep.uninhibit()
